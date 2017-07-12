d5ce2af51c51e5d00cad69ac1e96fc4fd13528e9
Mukund Ananthu <mukund.code@gmail.com>
2017-07-12 19:48:08 +0530

    14 done

---
+++ LocalizationOverview/14.py
@@ -0,0 +1,16 @@
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'green'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(p)):
+        q[i]=q[i]/s
+    return q
+print sense(p, Z)
